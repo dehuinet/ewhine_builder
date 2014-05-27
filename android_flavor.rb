afc01c46b37fa31cb53120c89710aa4afb15487f
vulgarcoder <vulgarcoder@gmail.com>
2014-05-27 18:11:38 +0800

    Enable merge androidManifest file.

--- android_flavor.rb
+++ android_flavor.rb
@@ -1,5 +1,6 @@
 require 'yaml'
-
+require 'ox'
+require 'pry'
 root=ARGV[0]
 flavor_name=ARGV[1]
 version_code=ARGV[2]
@@ -60,3 +61,22 @@ gradle_file_content.gsub!(/versionCode.*\n/,"versionCode #{version_code}\n")
 gradle_file_content.gsub!(/versionName.*\n/,"versionName \"#{version_name}\"\n")
 
 File.open(gradle_file, 'w') { |file| file.write(gradle_file_content) }
+
+build_file="#{root}/enterprise_micro_blog/flavors/#{flavors.first}/build.yaml" 
+if File.exists? build_file then
+puts build_file
+	build_config=YAML::load(File.read(build_file))
+	manifest_file="#{root}/enterprise_micro_blog/AndroidManifest.xml"
+
+	manifest_object = Ox.parse(File.read(manifest_file))
+	meta_objects=manifest_object.locate("application/meta-data")
+	meta_objects.each do|meta|
+		name=meta[:"android:name"]
+		puts build_config
+		puts build_config[name]
+		if build_config[name] then
+			meta[:"android:value"]=build_config[name]
+		end
+	end
+	File.open(manifest_file, 'w') { |file| file.write( Ox.dump(manifest_object)) }
+end
