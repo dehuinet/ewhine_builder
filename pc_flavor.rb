2182e538aac67a9a2227c7459aa64f82e4a50067
vulgarcoder <vulgarcoder@gmail.com>
2014-08-04 14:31:44 +0800

    change app config when building mac platform

--- pc_flavor.rb
+++ pc_flavor.rb
@@ -20,7 +20,10 @@ end
 
 
 config = YAML::load(File.read("#{flavors}/build/config.yaml"))
-
+if "mac"==platform then
+    config["app_id"]="4"
+    config["app_secret"]="df5042da190e040beaffee92b7a22c7e"
+end
 config_file = File.open("#{root}/app/scripts/config.js","w+")
 config_tmp = File.read("#{root}/config.js.erb")
 
